4f173fa9bf5f142f74678539a9ea4552b2711979
MrUniverse44 <oficial.mruniverse@gmail.com>
2023-12-09 09:58:14 -0500

    feat: added metrics

--- src/main/java/me/blueslime/minedis/Minedis.java
+++ src/main/java/me/blueslime/minedis/Minedis.java
@@ -6,12 +6,14 @@ import me.blueslime.minedis.modules.discord.Controller;
 import me.blueslime.minedis.modules.extensions.Extensions;
 import me.blueslime.minedis.modules.listeners.Listeners;
 import me.blueslime.minedis.modules.plugin.Plugin;
+import me.blueslime.minedis.shared.bstats.Metrics;
 
 public class Minedis extends Plugin {
 
     @Override
     public void onEnable() {
         initialize(this);
+        new Metrics(this, 20474);
     }
 
     @Override
@@ -22,11 +24,11 @@ public class Minedis extends Plugin {
     @Override
     public void registerModules() {
         registerModule(
-                new MinedisAPI(this),
-                new Commands(this),
-                new Listeners(this),
-                new Extensions(this),
-                new Controller(this)
+            new MinedisAPI(this),
+            new Commands(this),
+            new Listeners(this),
+            new Extensions(this),
+            new Controller(this)
         ).finish();
     }
 }
